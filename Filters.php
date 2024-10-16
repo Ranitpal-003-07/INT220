fc12a656453c720866ba0b87dcec4531cbacd938
Ranitpal-003-07 <ranitpal699@gmail.com>
2024-10-16 10:54:01 +0530

    updated files

--- Filters.php
+++ Filters.php
@@ -5,7 +5,7 @@ echo $newstr;
 echo"<br>";
 
 $int=100;
-if(filter_var($int, FILTER_VALIDATE_INT)===false)
+if(!filter_var($int, FILTER_VALIDATE_INT)===false)
 {
     echo "Integer Is Valid";
 }else{
@@ -24,6 +24,29 @@ if(filter_var($int,FILTER_VALIDATE_INT)===0 || !filter_var($int,FILTER_VALIDATE_
 echo "<br>";
 
 $url="https://google.com";
+$url=filter_var($url,FILTER_SANITIZE_URL);
+if(!filter_var($url,FILTER_VALIDATE_URL)===false){
+    echo "URL Is Valid";
+}else {
+    echo "URL Is Not Valid";
+}
+echo "<br>";
 
+$email="abc.232342@yohoo.com";
+$email=filter_var($email,FILTER_SANITIZE_EMAIL);
+if(!filter_var($email,FILTER_VALIDATE_EMAIL)===false){
+    echo "$email is a valid email address";
+}else{
+    echo "$email is not a valid email address";
+}
+echo "<br>";
+
+
+$ip="127.0.0.1";
+if(!filter_var($ip,FILTER_VALIDATE_IP)===false){
+    echo "$ip is a valid email address";
+}else{
+    echo "$ip is not a valid email addresss";
+}
 
 ?>
