0286fdccc3ff393ad61100790948f128215fbe9d
Ranitpal-003-07 <ranitpal699@gmail.com>
2024-10-18 15:18:47 +0530

    updated files

---
+++ FileUpload.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <form method="post" enctype="multipart/form-data" action="Receive.php">
+            <input type="file" name="myfile">
+            <input type="submit" name="upload" id="SEND">
+        </form>
+    </body>
+</html>
